c66ecebcc4193356fd50a5c93b926a5f8ba8ab20
Greg1709 <44266426+Greg1709@users.noreply.github.com>
2018-11-14 14:02:12 +0100

    Add files via upload
    V1.0

---
+++ Arduino Coeur de led/coeur.h
@@ -0,0 +1,11 @@
+#include "main.c"
+#define TEMPS 500; 
+//int led_rouge[]={1,2,3,4,5,6,7,8,9,10};
+int temps = 500;
+//int i; 
+int n;   
+void toutes_leds(int i, int led_rouge[], int temps);
+void chenille();
+void led_sur_deux();
+void led_sur_trois();
+void Une_LED_au_choix(int n);
