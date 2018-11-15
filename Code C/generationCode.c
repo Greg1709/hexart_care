732c2ce87e41db683c315456577dc0769adeb1ac
Greg1709 <44266426+Greg1709@users.noreply.github.com>
2018-11-16 00:18:37 +0100

    Add files via upload

--- Code C/generationCode.c
+++ Code C/generationCode.c
@@ -4,11 +4,11 @@ void envoyerparam(char mode, char led)
 
     FILE* fichier = NULL;
     // On définit l'emplacement du param.h :
-    fichier = fopen("app/param.h", "w");
+    fichier = fopen("param.h", "w");
     if(fichier != NULL)
     {
         //On écrit dans param.h le mode et la LED choisit par l'utilisateur   :
-        fprintf(fichier,"#define MODE %d \n#define LED %d",atoi(&mode),atoi(&led)); // Une convertion de char vers int et restitution
+        fprintf(fichier,"#define MODE %d \n#define CHOIX %d",atoi(&mode),atoi(&led)); // Une convertion de char vers int et restitution
 
         fclose(fichier);
     }
--- Code C/menu.c
+++ Code C/menu.c
@@ -6,7 +6,7 @@ void menu(char * choiceUser, char * scdChoice)
     // Affichage du menu:
     printf("--------- Veuillez choisir votre d'affichage :  --------- \n 1.Allumer toutes les Leds \n 2. 1 Led sur 2 \n 3. 1 Led sur 3 \n 4. Chenillard \n 5. Une led aux choix  \n ");
     scanf("%s", choiceUser);
-    checkNumber(choiceUser);
+
     while (atoi(choiceUser) <1 || atoi(choiceUser) > 5 || checkNumber(choiceUser) != 1) // Boucle qui demande à l'utilisateur de rentrer un nombre correct
     {
         printf("Veuillez choisir un nombre correspondant au menu \n");
@@ -15,11 +15,15 @@ void menu(char * choiceUser, char * scdChoice)
 
     if(atoi(choiceUser) == 5 || checkNumber(scdChoice) != 1) //Choix d'une LED à allumer
     {
-        do
-        {
-            printf("Attention vous devez choisir un nombre inf%crieur %c 10 ou sup%crieur %c 0 \n",130,133,130,133);
-            scanf("%s", scdChoice);
-        }while(*scdChoice<10 && *scdChoice>0);
+        scanf("%s", scdChoice);
+        while (atoi(scdChoice) <1 || atoi(scdChoice) > 10 || checkNumber(choiceUser) != 1) {
+
+                printf("Veuillez choisir un nombre correspondant au menu \n");
+                scanf("%s", scdChoice);
+
+
+        }
+
     }
 }
 
